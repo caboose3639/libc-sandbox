2ae21e6c1f2e6d5146175291af0cc469e339b540
abhishek <abhishek15@iisc.ac.in>
2025-10-21 11:14:14 +0530

    libc call mappings to dummy syscall args

---
+++ llvm-passes/scripts/dummy_syscall.py
@@ -0,0 +1,27 @@
+import subprocess
+import os
+
+command = "nm -D /usr/lib/x86_64-linux-gnu/libc.so.6 | grep ' T ' | awk '{print $3}' | cut -d@ -f1 | sort -u"
+
+result = subprocess.run(command, shell = True, capture_output = True, text = True)
+
+callnames = result.stdout.splitlines()
+callnames.sort()
+
+call_map = {name: i for i, name in enumerate(callnames)}
+
+header = []
+header.append("#pragma once")
+header.append("#include <string>")
+header.append("#include <unordered_map>")
+header.append("")
+header.append("static const std::unordered_map<std::string, int> dummy_syscall_map = {")
+for name in callnames:
+    header.append(f"    {{\"{name}\", {call_map[name]}}},")
+header.append("};")
+header.append("")
+
+out_path = "../include/dummy_syscall.h"
+
+with open(out_path, "w") as f: 
+    f.write("\n".join(header))
