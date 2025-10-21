345b6fc5c5938fbad73d0d2ab861f63953447cc3
abhishek <abhishek15@iisc.ac.in>
2025-10-21 17:28:26 +0530

    passes for syscall instrumentation & new cfg

---
+++ llvm-passes/src/InstrumentDummySyscall.cpp
@@ -0,0 +1,86 @@
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+
+#include <string>
+
+#include "../include/dummy_syscall.h"
+static int libcMap(const std::string& funcName) {
+    auto it = dummy_syscall_map.find(funcName);
+    if (it != dummy_syscall_map.end()) {
+        return it->second;
+    }
+    return -1;
+}
+
+namespace instrument {
+    class InstrumentPass : public llvm::PassInfoMixin<InstrumentPass> {
+        public:
+            static bool isRequired() {return true;}
+            bool instrumentSyscall(llvm::Module &Mod, llvm::FunctionCallee syscallFunc);
+            llvm::PreservedAnalyses run(llvm::Module &Mod, llvm::AnalysisManager<llvm::Module> &mngr);
+    };
+
+    llvm::PreservedAnalyses InstrumentPass::run(llvm::Module &Mod, llvm::AnalysisManager<llvm::Module> &mngr) {
+        llvm::LLVMContext &context = Mod.getContext();
+        llvm::Type *i32 = llvm::Type::getInt32Ty(context);
+        llvm::FunctionType *syscallFuncType = llvm::FunctionType::get(llvm::Type::getVoidTy(context), {i32}, false);
+        llvm::FunctionCallee syscallFunc = Mod.getOrInsertFunction("dummy_syscall", syscallFuncType);
+        bool modified = instrumentSyscall(Mod, syscallFunc);
+   
+        return modified ? llvm::PreservedAnalyses::none() : llvm::PreservedAnalyses::all();
+    }
+
+    bool InstrumentPass::instrumentSyscall(llvm::Module &Mod, llvm::FunctionCallee syscallFunc) {
+        bool modified = false;
+
+        llvm::IRBuilder<> Builder(Mod.getContext());
+        for (llvm::Function &func : Mod) {
+            if(func.getName() == "dummy_syscall")
+                continue;
+
+            for(llvm::BasicBlock &bb : func) {
+                for(llvm::Instruction &inst : bb) {
+                    if(llvm::CallInst *callInst = llvm::dyn_cast<llvm::CallInst>(&inst)) {
+                        llvm::Function *calledFunc = callInst->getCalledFunction();
+                        if(calledFunc) {
+                            std::string funcName = calledFunc->getName().str();
+                            int syscallArg = libcMap(funcName);
+                            if(syscallArg >= 0) {
+                                Builder.SetInsertPoint(callInst);
+                                llvm::Value *arg = llvm::ConstantInt::get(llvm::Type::getInt32Ty(Mod.getContext()), syscallArg);
+                                Builder.CreateCall(syscallFunc, {arg});
+                                modified = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return modified;
+    }
+};
+
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+    return {
+        LLVM_PLUGIN_API_VERSION, "InstrumentPass", "v0.1",
+        [](llvm::PassBuilder &PB) {
+            PB.registerPipelineParsingCallback(
+                [](llvm::StringRef Name, llvm::ModulePassManager &MPM,
+                   llvm::ArrayRef<llvm::PassBuilder::PipelineElement>) {
+                    if(Name == "instrument-pass") {
+                        MPM.addPass(instrument::InstrumentPass());
+                        return true;
+                    }
+                    return false;
+                }
+            );
+        }
+    };
+}
